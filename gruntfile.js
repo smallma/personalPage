b1b72f3901d7a843e5c90760941c8f54ac368447
Rain Lin <s.rain@yahoo.com.tw>
2015-04-13 22:16:23 +0800

    * add grunt, sass, jslint

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'sass',
+          src: ['*.sass'],
+          dest: 'css',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: 'sass/*.sass',
+        tasks: ['sass']
+      },
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'sass']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['jshint', 'sass']);
+
+};
